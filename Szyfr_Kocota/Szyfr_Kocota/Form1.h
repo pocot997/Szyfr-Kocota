38e417a8deda0656b491540ef7d72aa45dc138ce
Krzysztof Kocot <krzychu.kocot@gmail.com>
2020-10-15 15:27:19 +0200

    GUI

--- Szyfr_Kocota/Szyfr_Kocota/Form1.h
+++ Szyfr_Kocota/Szyfr_Kocota/Form1.h
@@ -1,6 +1,7 @@
 #pragma once
 
-namespace CppCLRWinformsProjekt {
+namespace CppCLRWinformsProjekt
+{
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -14,7 +15,7 @@ namespace CppCLRWinformsProjekt {
 	/// </summary>
 	public ref class Form1 : public System::Windows::Forms::Form
 	{
-	public:
+		public:
 		Form1(void)
 		{
 			InitializeComponent();
@@ -23,7 +24,7 @@ namespace CppCLRWinformsProjekt {
 			//
 		}
 
-	protected:
+		protected:
 		/// <summary>
 		/// Verwendete Ressourcen bereinigen.
 		/// </summary>
@@ -34,26 +35,18 @@ namespace CppCLRWinformsProjekt {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::RadioButton^ radioCpp;
-	private: System::Windows::Forms::RadioButton^ radioAsm;
-	private: System::Windows::Forms::Button^ buttonEncode;
-	private: System::Windows::Forms::Button^ buttonDecode;
-	private: System::Windows::Forms::TextBox^ cipherTextBox;
-	private: System::Windows::Forms::Button^ ChessButton;
-
-	private: System::Windows::Forms::Button^ saveFileButton;
-	private: System::Windows::Forms::Button^ showFileButton;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
-	private: System::Windows::Forms::Label^ threadsLabel;
-	private: System::Windows::Forms::TextBox^ infoTextBox;
-	private: System::Windows::Forms::TextBox^ textBox1;
-	protected:
-
-	protected:
-
-
-	private:
+		private: System::Windows::Forms::RadioButton^ radio_cpp;
+		private: System::Windows::Forms::RadioButton^ radio_asm;
+		private: System::Windows::Forms::Button^ button_encode;
+		private: System::Windows::Forms::Button^ button_decode;
+		private: System::Windows::Forms::TextBox^ text_box_right;
+		private: System::Windows::Forms::Button^ button_open_key;
+		private: System::Windows::Forms::Button^ button_save_text;
+		private: System::Windows::Forms::Button^ button_open_text;
+		private: System::Windows::Forms::Button^ button_set_threads;
+		private: System::Windows::Forms::NumericUpDown^ numeric_up_down_threads;
+		private: System::Windows::Forms::TextBox^ text_box_middle;
+		private: System::Windows::Forms::TextBox^ text_box_left;
 		/// <summary>
 		/// Erforderliche Designervariable.
 		/// </summary>
@@ -66,180 +59,212 @@ namespace CppCLRWinformsProjekt {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->radioCpp = (gcnew System::Windows::Forms::RadioButton());
-			this->radioAsm = (gcnew System::Windows::Forms::RadioButton());
-			this->buttonEncode = (gcnew System::Windows::Forms::Button());
-			this->buttonDecode = (gcnew System::Windows::Forms::Button());
-			this->cipherTextBox = (gcnew System::Windows::Forms::TextBox());
-			this->ChessButton = (gcnew System::Windows::Forms::Button());
-			this->saveFileButton = (gcnew System::Windows::Forms::Button());
-			this->showFileButton = (gcnew System::Windows::Forms::Button());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
-			this->threadsLabel = (gcnew System::Windows::Forms::Label());
-			this->infoTextBox = (gcnew System::Windows::Forms::TextBox());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
+			this->radio_cpp = (gcnew System::Windows::Forms::RadioButton());
+			this->radio_asm = (gcnew System::Windows::Forms::RadioButton());
+			this->button_encode = (gcnew System::Windows::Forms::Button());
+			this->button_decode = (gcnew System::Windows::Forms::Button());
+			this->text_box_right = (gcnew System::Windows::Forms::TextBox());
+			this->button_open_key = (gcnew System::Windows::Forms::Button());
+			this->button_save_text = (gcnew System::Windows::Forms::Button());
+			this->button_open_text = (gcnew System::Windows::Forms::Button());
+			this->button_set_threads = (gcnew System::Windows::Forms::Button());
+			this->numeric_up_down_threads = (gcnew System::Windows::Forms::NumericUpDown());
+			this->text_box_middle = (gcnew System::Windows::Forms::TextBox());
+			this->text_box_left = (gcnew System::Windows::Forms::TextBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numeric_up_down_threads))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// radioCpp
+			// radio_cpp
 			// 
-			this->radioCpp->AutoSize = true;
-			this->radioCpp->Location = System::Drawing::Point(367, 84);
-			this->radioCpp->Name = L"radioCpp";
-			this->radioCpp->Size = System::Drawing::Size(44, 17);
-			this->radioCpp->TabIndex = 0;
-			this->radioCpp->TabStop = true;
-			this->radioCpp->Text = L"C++";
-			this->radioCpp->UseVisualStyleBackColor = true;
-			this->radioCpp->CheckedChanged += gcnew System::EventHandler(this, &Form1::radioButton1_CheckedChanged);
+			this->radio_cpp->AutoSize = true;
+			this->radio_cpp->Location = System::Drawing::Point(375, 86);
+			this->radio_cpp->Name = L"radio_cpp";
+			this->radio_cpp->Size = System::Drawing::Size(44, 17);
+			this->radio_cpp->TabIndex = 0;
+			this->radio_cpp->TabStop = true;
+			this->radio_cpp->Text = L"C++";
+			this->radio_cpp->UseVisualStyleBackColor = true;
+			this->radio_cpp->CheckedChanged += gcnew System::EventHandler(this, &Form1::radioButton1_CheckedChanged);
 			// 
-			// radioAsm
+			// radio_asm
 			// 
-			this->radioAsm->AutoSize = true;
-			this->radioAsm->Location = System::Drawing::Point(366, 109);
-			this->radioAsm->Name = L"radioAsm";
-			this->radioAsm->Size = System::Drawing::Size(45, 17);
-			this->radioAsm->TabIndex = 1;
-			this->radioAsm->TabStop = true;
-			this->radioAsm->Text = L"Asm";
-			this->radioAsm->UseVisualStyleBackColor = true;
+			this->radio_asm->AutoSize = true;
+			this->radio_asm->Location = System::Drawing::Point(374, 111);
+			this->radio_asm->Name = L"radio_asm";
+			this->radio_asm->Size = System::Drawing::Size(45, 17);
+			this->radio_asm->TabIndex = 1;
+			this->radio_asm->TabStop = true;
+			this->radio_asm->Text = L"Asm";
+			this->radio_asm->UseVisualStyleBackColor = true;
+			this->radio_asm->CheckedChanged += gcnew System::EventHandler(this, &Form1::radio_asm_CheckedChanged);
 			// 
-			// buttonEncode
+			// button_encode
 			// 
-			this->buttonEncode->Location = System::Drawing::Point(417, 109);
-			this->buttonEncode->Name = L"buttonEncode";
-			this->buttonEncode->Size = System::Drawing::Size(75, 23);
-			this->buttonEncode->TabIndex = 2;
-			this->buttonEncode->Text = L"Encode";
-			this->buttonEncode->UseVisualStyleBackColor = true;
+			this->button_encode->Location = System::Drawing::Point(425, 111);
+			this->button_encode->Name = L"button_encode";
+			this->button_encode->Size = System::Drawing::Size(75, 23);
+			this->button_encode->TabIndex = 2;
+			this->button_encode->Text = L"Encode";
+			this->button_encode->UseVisualStyleBackColor = true;
+			this->button_encode->Click += gcnew System::EventHandler(this, &Form1::button_encode_Click);
 			// 
-			// buttonDecode
+			// button_decode
 			// 
-			this->buttonDecode->Location = System::Drawing::Point(417, 81);
-			this->buttonDecode->Name = L"buttonDecode";
-			this->buttonDecode->Size = System::Drawing::Size(75, 23);
-			this->buttonDecode->TabIndex = 3;
-			this->buttonDecode->Text = L"Decode";
-			this->buttonDecode->UseVisualStyleBackColor = true;
+			this->button_decode->Location = System::Drawing::Point(425, 83);
+			this->button_decode->Name = L"button_decode";
+			this->button_decode->Size = System::Drawing::Size(75, 23);
+			this->button_decode->TabIndex = 3;
+			this->button_decode->Text = L"Decode";
+			this->button_decode->UseVisualStyleBackColor = true;
+			this->button_decode->Click += gcnew System::EventHandler(this, &Form1::button_decode_Click);
 			// 
-			// cipherTextBox
+			// text_box_right
 			// 
-			this->cipherTextBox->Location = System::Drawing::Point(588, 11);
-			this->cipherTextBox->Multiline = true;
-			this->cipherTextBox->Name = L"cipherTextBox";
-			this->cipherTextBox->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
-			this->cipherTextBox->Size = System::Drawing::Size(272, 264);
-			this->cipherTextBox->TabIndex = 26;
+			this->text_box_right->Location = System::Drawing::Point(588, 12);
+			this->text_box_right->Multiline = true;
+			this->text_box_right->Name = L"text_box_right";
+			this->text_box_right->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->text_box_right->Size = System::Drawing::Size(272, 272);
+			this->text_box_right->TabIndex = 26;
+			this->text_box_right->TextChanged += gcnew System::EventHandler(this, &Form1::text_box_right_TextChanged);
 			// 
-			// ChessButton
+			// button_open_key
 			// 
-			this->ChessButton->Location = System::Drawing::Point(390, 253);
-			this->ChessButton->Name = L"ChessButton";
-			this->ChessButton->Size = System::Drawing::Size(96, 23);
-			this->ChessButton->TabIndex = 25;
-			this->ChessButton->Text = L"Open key file";
-			this->ChessButton->UseVisualStyleBackColor = true;
+			this->button_open_key->Location = System::Drawing::Point(390, 260);
+			this->button_open_key->Name = L"button_open_key";
+			this->button_open_key->Size = System::Drawing::Size(96, 23);
+			this->button_open_key->TabIndex = 25;
+			this->button_open_key->Text = L"Open key file";
+			this->button_open_key->UseVisualStyleBackColor = true;
+			this->button_open_key->Click += gcnew System::EventHandler(this, &Form1::button_open_key_Click);
 			// 
-			// saveFileButton
+			// button_save_text
 			// 
-			this->saveFileButton->Location = System::Drawing::Point(493, 253);
-			this->saveFileButton->Name = L"saveFileButton";
-			this->saveFileButton->Size = System::Drawing::Size(89, 23);
-			this->saveFileButton->TabIndex = 23;
-			this->saveFileButton->Text = L"Save text file";
-			this->saveFileButton->UseVisualStyleBackColor = true;
+			this->button_save_text->Location = System::Drawing::Point(493, 260);
+			this->button_save_text->Name = L"button_save_text";
+			this->button_save_text->Size = System::Drawing::Size(89, 23);
+			this->button_save_text->TabIndex = 23;
+			this->button_save_text->Text = L"Save text file";
+			this->button_save_text->UseVisualStyleBackColor = true;
+			this->button_save_text->Click += gcnew System::EventHandler(this, &Form1::button_save_text_Click);
 			// 
-			// showFileButton
+			// button_open_text
 			// 
-			this->showFileButton->Location = System::Drawing::Point(290, 253);
-			this->showFileButton->Name = L"showFileButton";
-			this->showFileButton->Size = System::Drawing::Size(94, 23);
-			this->showFileButton->TabIndex = 22;
-			this->showFileButton->Text = L"Open text file";
-			this->showFileButton->UseVisualStyleBackColor = true;
+			this->button_open_text->Location = System::Drawing::Point(290, 260);
+			this->button_open_text->Name = L"button_open_text";
+			this->button_open_text->Size = System::Drawing::Size(94, 23);
+			this->button_open_text->TabIndex = 22;
+			this->button_open_text->Text = L"Open text file";
+			this->button_open_text->UseVisualStyleBackColor = true;
+			this->button_open_text->Click += gcnew System::EventHandler(this, &Form1::button_open_text_Click);
 			// 
-			// button1
+			// button_set_threads
 			// 
-			this->button1->Location = System::Drawing::Point(445, 11);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 20);
-			this->button1->TabIndex = 29;
-			this->button1->Text = L"Set threads";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			this->button_set_threads->Location = System::Drawing::Point(425, 10);
+			this->button_set_threads->Name = L"button_set_threads";
+			this->button_set_threads->Size = System::Drawing::Size(75, 20);
+			this->button_set_threads->TabIndex = 29;
+			this->button_set_threads->Text = L"Set threads";
+			this->button_set_threads->UseVisualStyleBackColor = true;
+			this->button_set_threads->Click += gcnew System::EventHandler(this, &Form1::button_set_threads_Click);
 			// 
-			// numericUpDown1
+			// numeric_up_down_threads
 			// 
-			this->numericUpDown1->Location = System::Drawing::Point(393, 11);
-			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 64, 0, 0, 0 });
-			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
-			this->numericUpDown1->Name = L"numericUpDown1";
-			this->numericUpDown1->Size = System::Drawing::Size(46, 20);
-			this->numericUpDown1->TabIndex = 28;
-			this->numericUpDown1->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numeric_up_down_threads->Location = System::Drawing::Point(375, 10);
+			this->numeric_up_down_threads->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1024, 0, 0, 0 });
+			this->numeric_up_down_threads->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numeric_up_down_threads->Name = L"numeric_up_down_threads";
+			this->numeric_up_down_threads->Size = System::Drawing::Size(46, 20);
+			this->numeric_up_down_threads->TabIndex = 28;
+			this->numeric_up_down_threads->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->numeric_up_down_threads->ValueChanged += gcnew System::EventHandler(this, &Form1::numeric_up_down_threads_ValueChanged);
 			// 
-			// threadsLabel
+			// text_box_middle
 			// 
-			this->threadsLabel->AutoSize = true;
-			this->threadsLabel->Location = System::Drawing::Point(338, 15);
-			this->threadsLabel->Name = L"threadsLabel";
-			this->threadsLabel->Size = System::Drawing::Size(49, 13);
-			this->threadsLabel->TabIndex = 27;
-			this->threadsLabel->Text = L"Threads:";
+			this->text_box_middle->Location = System::Drawing::Point(374, 36);
+			this->text_box_middle->Multiline = true;
+			this->text_box_middle->Name = L"text_box_middle";
+			this->text_box_middle->ScrollBars = System::Windows::Forms::ScrollBars::Both;
+			this->text_box_middle->Size = System::Drawing::Size(126, 41);
+			this->text_box_middle->TabIndex = 30;
+			this->text_box_middle->TextChanged += gcnew System::EventHandler(this, &Form1::text_box_middle_TextChanged);
 			// 
-			// infoTextBox
+			// text_box_left
 			// 
-			this->infoTextBox->Location = System::Drawing::Point(341, 37);
-			this->infoTextBox->Multiline = true;
-			this->infoTextBox->Name = L"infoTextBox";
-			this->infoTextBox->ScrollBars = System::Windows::Forms::ScrollBars::Both;
-			this->infoTextBox->Size = System::Drawing::Size(179, 41);
-			this->infoTextBox->TabIndex = 30;
-			// 
-			// textBox1
-			// 
-			this->textBox1->Location = System::Drawing::Point(12, 12);
-			this->textBox1->Multiline = true;
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
-			this->textBox1->Size = System::Drawing::Size(272, 264);
-			this->textBox1->TabIndex = 31;
+			this->text_box_left->Location = System::Drawing::Point(12, 12);
+			this->text_box_left->Multiline = true;
+			this->text_box_left->Name = L"text_box_left";
+			this->text_box_left->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->text_box_left->Size = System::Drawing::Size(272, 272);
+			this->text_box_left->TabIndex = 31;
+			this->text_box_left->TextChanged += gcnew System::EventHandler(this, &Form1::text_box_left_TextChanged);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(873, 600);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->infoTextBox);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->numericUpDown1);
-			this->Controls->Add(this->threadsLabel);
-			this->Controls->Add(this->cipherTextBox);
-			this->Controls->Add(this->ChessButton);
-			this->Controls->Add(this->saveFileButton);
-			this->Controls->Add(this->showFileButton);
-			this->Controls->Add(this->buttonDecode);
-			this->Controls->Add(this->buttonEncode);
-			this->Controls->Add(this->radioAsm);
-			this->Controls->Add(this->radioCpp);
+			this->ClientSize = System::Drawing::Size(873, 601);
+			this->Controls->Add(this->text_box_left);
+			this->Controls->Add(this->text_box_middle);
+			this->Controls->Add(this->button_set_threads);
+			this->Controls->Add(this->numeric_up_down_threads);
+			this->Controls->Add(this->text_box_right);
+			this->Controls->Add(this->button_open_key);
+			this->Controls->Add(this->button_save_text);
+			this->Controls->Add(this->button_open_text);
+			this->Controls->Add(this->button_decode);
+			this->Controls->Add(this->button_encode);
+			this->Controls->Add(this->radio_asm);
+			this->Controls->Add(this->radio_cpp);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
 			this->Name = L"Form1";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Form1";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numeric_up_down_threads))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void radioButton1_CheckedChanged(System::Object^ sender, System::EventArgs^ e)
-	{
-	}
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e)
-	{
-	}
-private: System::Void plainTextBox_TextChanged(System::Object^ sender, System::EventArgs^ e)
-	{
-	}
-};
+		private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void numeric_up_down_threads_ValueChanged(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void radioButton1_CheckedChanged(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void radio_asm_CheckedChanged(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void text_box_left_TextChanged(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void text_box_middle_TextChanged(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void text_box_right_TextChanged(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void button_set_threads_Click(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void button_decode_Click(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void button_encode_Click(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void button_open_text_Click(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void button_open_key_Click(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+		private: System::Void button_save_text_Click(System::Object^ sender, System::EventArgs^ e)
+		{
+		}
+	};
 }
